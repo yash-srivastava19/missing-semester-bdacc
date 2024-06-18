7dce7a6588a95ecde325821b67776bdf59d0773c
pavanKarthik2006 <aspkarthik.nitw@gmail.com>
2024-06-19 00:08:57 +0530

    Update fibonacci.py
    Changed into an Iterative code as recursive code had implementation errors

--- git/python/fibonacci.py
+++ git/python/fibonacci.py
@@ -7,8 +7,10 @@ def fibonacci(n):
         return 0
     elif n == 1:
         return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        a,b=0,1
+    for i in range(2,n+1):
+        a,b=b,a+b\
+    return b
 
 # Call the function
 print(fibonacci(35))  # Hint: This may take a long time or cause a RecursionError
