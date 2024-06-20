20d88497c430a0510d0aab16e395dce342b36537
pavanKarthik2006 <aspkarthik.nitw@gmail.com>
2024-06-20 22:06:05 +0530

    Update fibonacci.py
    Sorry didn't see the typo . Fixed the bug

--- git/python/fibonacci.py
+++ git/python/fibonacci.py
@@ -7,9 +7,11 @@ def fibonacci(n):
         return 0
     elif n == 1:
         return 1
-        a,b=0,1
-    for i in range(2,n+1):
-        a,b=b,a+b\
+    
+    a, b = 0, 1
+    for i in range(2, n + 1):
+        a, b = b, a + b
+    
     return b
 
 # Call the function
