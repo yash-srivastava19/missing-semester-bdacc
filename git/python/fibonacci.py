17239f4c423ae350129d0bf90486aa1c71c83ed3
Kammula Keerthi <128952654+Keerthi29062005@users.noreply.github.com>
2024-06-27 16:19:09 +0530

    Fix python syntax error (#11)
    * Optimised python code
    
    * Optimised python code using Dynamic programming

--- git/python/fibonacci.py
+++ git/python/fibonacci.py
@@ -3,12 +3,18 @@
 # Hint: This implementation is inefficient for large values of n
 
 def fibonacci(n):
+    a=0
+    b=1
     if n <= 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+        return b
+#Used Dynamic programming to optimise the code
 # Call the function
 print(fibonacci(35))  # Hint: This may take a long time or cause a RecursionError
