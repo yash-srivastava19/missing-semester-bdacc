acc08a832554dc2da54a8eac3acad779a512d0a0
Mohammed Mohtasim .M.S <143943473+003falcon@users.noreply.github.com>
2024-06-27 16:26:02 +0530

    Fix-js-logical_error (#12)
    * Modified multiply function
    
    * Modified divide function,added another function to work inplace of returning -1
    
    * Modified int main function and added a header file
    
    * changed upperlimit of forloop

--- git/js/array_sum.js
+++ git/js/array_sum.js
@@ -4,7 +4,7 @@
 
 function arraySum(arr) {
     let sum = 0;
-    for (let i = 0; i < arr.length -1 ; i++) {
+    for (let i = 0; i <= arr.length -1 ; i++) {
         sum += arr[i];
     }
     return sum;
@@ -15,4 +15,4 @@ const numbers = [1, 2, 3, 4, 5];
 const result = arraySum(numbers);
 console.log(`The sum of ${numbers} is ${result}`);
 
-// Hint: The output may not be as expected
+
